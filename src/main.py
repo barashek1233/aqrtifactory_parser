d59d89843ee1cdb6b3d8bd2dc06c7d0381b949ec
Войновский Илья Олегович <i.voynovskiy@aqsi.ru>
2024-06-25 00:15:08 +0300

    main

--- src/main.py
+++ src/main.py
@@ -12,22 +12,22 @@ def get_respons_from_url(url):
     try:
         respons = requests.get(url, timeout=10)
     except requests.exceptions.RequestException as e:
-        logs.error(f"A request error occurred: {e}")
+        logs.error(f"get_respons_from_url | A request error occurred: {e}")
         respons = None
     except requests.exceptions.ConnectionError as e:
-        logs.error(f"Connection error: {e}")
+        logs.error(f"get_respons_from_url | Connection error: {e}")
         respons = None
     except requests.exceptions.Timeout as e:
-        logs.error(f"Timeout exceeded: {e}")
+        logs.error(f"get_respons_from_url | Timeout exceeded: {e}")
         respons = None
     except requests.exceptions.TooManyRedirects as e:
-        logs.error(f"Too many redirects: {e}")
+        logs.error(f"get_respons_from_url | Too many redirects: {e}")
         respons = None
     except requests.exceptions.HTTPError as e:
-        logs.error(f"HTTP error: {e}")
+        logs.error(f"get_respons_from_url | HTTP error: {e}")
         respons = None
     else:
-        logs.info("Request completed successfully")
+        logs.info("get_respons_from_url | Request completed successfully")
     return respons
 
 
@@ -36,15 +36,15 @@ def get_all_links_from_respons(response):
         soup = BeautifulSoup(response.text, 'html.parser')
         links = [link.get('href') for link in soup.find_all('a', href=True)]
     except AttributeError as e:
-        logs.error(f"Error getting href: {e}")
+        logs.error(f"get_all_links_from_respons| Error getting href: {e}")
         links = None
     except Exception as e:
-        logs.error(f"Other error: {e}")
+        logs.error(f"get_all_links_from_respons | Other error: {e}")
         links = None
     else:
-        logs.info(f"Done")
+        logs.info(f"get_all_links_from_respons | Done")
         
-    logs.debug(f"links: {links}")
+    logs.debug(f"get_all_links_from_respons | links: {links}")
     return links
 
 
@@ -53,10 +53,10 @@ def getting_links(URL : str, cube_name : str):
     response = get_respons_from_url(URL)
 
     if response is not None:
-        logs.info(f"{cube_name} | Good Response, return links")
+        logs.info(f"getting_links | {cube_name} | Good Response, return links")
         links = get_all_links_from_respons(response)
     else:
-        logs.error(f"{cube_name} | Respons is None Error")
+        logs.error(f"getting_links | {cube_name} | Respons is None Error")
         links = None
 
     return links
@@ -67,13 +67,13 @@ def check_file_struct(file_struct : str):
         with open(file_struct, "r") as file:
             data = json.load(file)
     except FileNotFoundError:
-        logs.warning(f"File_struct: {file_struct} not found ALARM!!!!")  # вот тут надо буедет сделать логирование в телеграм потому что если этого файла нет программа запущена в первый раз или файл удалилс
+        logs.warning(f"check_file_struct | File_struct: {file_struct} not found ALARM!!!!")  # вот тут надо буедет сделать логирование в телеграм потому что если этого файла нет программа запущена в первый раз или файл удалилс
         data = {}
     else:
-        logs.info(f"File_struct: {file_struct} open")
+        logs.info(f" check_file_struct | File_struct: {file_struct} open")
     return data
 
-
+@logs.catch
 def search_new_links(links_list : list):
     """
     Фнукция делает get запрос по адресу указанному в параметре linls_list спику и возвращает список всех ссылкок по указанному адресу, 
@@ -89,10 +89,10 @@ def search_new_links(links_list : list):
     """
     links_str : str = ""
     itter = 0
-    logs.debug(f"links_list: {links_list}")
+    logs.debug(f"search_new_links | links_list: {links_list}")
     for link in links_list:
         if type(link) != str:
-            logs.debug(f"Error link, {link} is not type str/ WTF??")
+            logs.debug(f"search_new_links | Error link, {link} is not type str/ WTF??")
             return ["error", link, type(link), itter]
         else:
             links_str = links_str + link
@@ -101,46 +101,46 @@ def search_new_links(links_list : list):
     return_list_links_on_page : list | None = getting_links(links_str, links_str)
 
     if return_list_links_on_page is None:
-        logs.debug(f"return_list_links_on_page is None {return_list_links_on_page}")
+        logs.warning(f"search_new_links | return_list_links_on_page is None {return_list_links_on_page}")
         return return_list_links_on_page
     
     for link_on_page in return_list_links_on_page:
         if link_on_page == "../":
-            logs.debug(f"removing value: '{link_on_page}' from response")
+            logs.debug(f"search_new_links | removing value: '{link_on_page}' from response")
             return_list_links_on_page.remove(link_on_page)
 
     return return_list_links_on_page
         
 
-def populating_the_dictionary_with_get_queries(main_link : list, type_cube : str, data_from_file_struct : dict) -> dict:
+def populating_the_dictionary_with_get_queries(main_link : list, type_link : str, data_from_file_struct : dict) -> dict:
     test_dict = {}
     test_dict["sam-ba"] = ["1", "2", "3"]
     test_dict["sam-ba-100hz"] = ["1", "2", "3"]
     
-    main_link.append(type_cube)
-    logs.debug(f"main_link: {main_link}")
+    main_link.append(type_link)
+    logs.debug(f"populating_the_dictionary_with_get_queries | main_link: {main_link}")
     list_links_on_page : list | None = search_new_links(main_link)
-    logs.debug(f"list_links_on_page: {list_links_on_page}")
+    logs.debug(f"populating_the_dictionary_with_get_queries | list_links_on_page: {list_links_on_page}")
     if list_links_on_page is None:
         return list_links_on_page
     for item in list_links_on_page:
         # if item is not data_from_file_struct:
-        logs.debug(f"item is not data_from_file_struct: {item}")
+        logs.debug(f"populating_the_dictionary_with_get_queries | item is not data_from_file_struct: {item}")
         if item[-1] == "/":
-            logs.debug(f"item[-1] == / {item[-1]}")
+            logs.debug(f"item[-1] == /")
             data_from_file_struct[item] = {}
-            logs.debug(f"create item: {item} {data_from_file_struct[item]}")
+            logs.debug(f"populating_the_dictionary_with_get_queries | create item: {item} {data_from_file_struct[item]}")
             data_from_file_struct[item] = populating_the_dictionary_with_get_queries(main_link, item, data_from_file_struct[item])
-        else:
-            # написать фнукцию раскидывающую по версия самбы и сву пакеты пока типо она тут есть, и возвращает готовый словарь такого толка data_from_file_struct[type_cube] 
-            logs.debug(f"return test_dict")
+        elif ".bin" in item:  #  надо что то придумать с проверкой? потому что основная проблема тут. надо научиться разделять ссылки на файлы и сами файлы. а все остальное нахер
+            # написать фнукцию раскидывающую по версия самбы и сву пакеты пока типо она тут есть, и возвращает готовый словарь такого толка data_from_file_struct[type_link] 
+            logs.debug(f"populating_the_dictionary_with_get_queries | return test_dict because item: {item} and type_link: {type_link}")
             return test_dict
             # data_from_file_struct[item] = populating_the_dictionary_with_get_queries(main_link, item, data_from_file_struct[item])
         main_link.pop()
 
     return data_from_file_struct
                 
-    # file_struct : dict = data_from_file_struct[type_cube]
+    # file_struct : dict = data_from_file_struct[type_link]
 
     # if list_links_on_page is None:
     #     pass
